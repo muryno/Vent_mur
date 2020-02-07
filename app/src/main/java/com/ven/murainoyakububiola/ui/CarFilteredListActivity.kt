9cef6f3c8044cebaeeeaa2815a5a31985d5f5c62
muryno <murainoy@yahoo.com>
2020-02-07 01:24:33 +0100

    Base Activity to check if app is online or not else will display snacks  , CarFilteredActivity created

---
+++ app/src/main/java/com/ven/murainoyakububiola/ui/CarFilteredListActivity.kt
@@ -0,0 +1,12 @@
+package com.ven.murainoyakububiola.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CarFilteredListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_filtered_list)
+    }
+}
